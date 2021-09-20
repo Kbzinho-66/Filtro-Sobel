faef389da3832bea7eea57383fbacb6496e2c431
Kbzinho-66 <gvalles@ucs.br>
2021-09-19 23:09:59 -0300

    Começo da paralelização

--- sobel.c
+++ sobel.c
@@ -3,19 +3,24 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
 
 #pragma pack(1)
 
-#define CELL_A i-1][j-1
-#define CELL_B i-1][j
-#define CELL_C i-1][j+1
-#define CELL_D i][j-1
-#define CELL_E i][j
-#define CELL_F i][j+1
-#define CELL_G i+1][j-1
-#define CELL_H i+1][j
-#define CELL_I i+1][j+1
+#define CELL_A i-1 * h.largura + j-1
+#define CELL_B i-1 * h.largura + j
+#define CELL_C i-1 * h.largura + j+1
+#define CELL_D i   * h.largura + j-1
+#define CELL_E i   * h.largura + j
+#define CELL_F i   * h.largura + j+1
+#define CELL_G i+1 * h.largura + j-1
+#define CELL_H i+1 * h.largura + j
+#define CELL_I i+1 * h.largura + j+1
 
+#define greyscale(linha, coluna) greyscale[linha * h.largura + coluna]
+
+typedef unsigned char Byte;
 struct header {
 	unsigned short tipo;
 	unsigned int tamanho_arquivo;
@@ -37,141 +42,180 @@ struct header {
 typedef struct header Header;
 
 struct pixel{
-	unsigned char blue;
-	unsigned char green;
-	unsigned char red;
+	Byte blue;
+	Byte green;
+	Byte red;
 };
 typedef struct pixel Pixel;
 
+void converter_grayscale(Pixel **imagem, Byte **greyscale, int numProc, char *nomeArq);
+
+void calcular(Pixel **imagem, Byte **greyscale, int rank, int numProc);
+
+Header h;
+
 int main(int argc, char **argv){
 
-    Header h;
+	int numProc;
 	FILE *arquivoEntrada, *arquivoSaida;
 	arquivoEntrada = arquivoSaida = NULL;
-	char nomeArquivo[50], saida[100], nomeSemExtensao[50];
+	char nomeArquivo[50];
+	int i, j;
 
     if (argc == 3) {
-		// TODO Fazer a inicialização com processos e tudo
+		strcpy(nomeArquivo, argv[1]);
+		numProc = atoi(argv[2]);
     } else if (argc <= 1) {
-
-		strcpy(nomeArquivo, "Borboleta.bmp");
-		strcpy(nomeSemExtensao, nomeArquivo);
+		strcpy(nomeArquivo, "Barco.bmp");
+		numProc = 4;
 	}
-
-	nomeSemExtensao[strlen(nomeSemExtensao) - 4] = '\0';
-	strcpy(saida, nomeSemExtensao);
-	strcat(saida, "_Greyscale");
-
+	
 	arquivoEntrada = fopen(nomeArquivo, "rb");
 	if (arquivoEntrada == NULL) {
         printf("Erro ao abrir a imagem de entrada.\n");
         exit(0);
 	}
 
-	arquivoSaida = fopen(saida, "wb");
-	if (arquivoSaida == NULL) {
-		printf("Erro ao salvar a imagem em tons de cinza.\n");
-		exit(0);
-	}
-	
-	fread(&h, sizeof(Header), 1, arquivoEntrada);
+	fread(&h, sizeof(Header), 1, arquivoEntrada);	
 
-	int i, j;
-	unsigned char grey;
+	int chaveShmGreyscale = 13, idShmGreyscale;
+	// int chaveShmSobel = 17, idShmSobel;
 
 	// Criar as matriz de Pixels e o valor Greyscale de cada Pixel
-	// TODO Criar em uma área de memória compartilhada
 	Pixel **imagem = (Pixel**) malloc(h.altura * sizeof(Pixel *));
-	unsigned char **greyscale = (unsigned char**) malloc(h.altura * sizeof(unsigned char *));
-	unsigned char **sobel = (unsigned char**) malloc(h.altura * sizeof(unsigned char *));
+	
+	
+	idShmGreyscale = shmget(chaveShmGreyscale, h.altura * h.largura, 0600 | IPC_CREAT);
+	Byte **greyscale = shmat(idShmGreyscale, 0, 0);
 
-	for(i=0; i<h.altura; i++) {
-		imagem[i] = (Pixel*) malloc(h.largura * sizeof(Pixel)); 
-		greyscale[i] = (unsigned char*) malloc(h.largura * sizeof(unsigned char));
-		sobel[i] = (unsigned char*) malloc(h.largura * sizeof(unsigned char));
+
+	for (i = 0; i < h.altura; i++) {
+		imagem[i] = (Pixel*) malloc (h.largura * sizeof(Pixel));
+		greyscale[i] = (Byte*) malloc (h.largura * sizeof(Byte));
 	}
+	// idShmSobel = shmget(chaveShmSobel, h.altura * h.largura, 0600 | IPC_CREAT);
+	// Byte *sobel = shmat(idShmSobel, 0, 0);
 
 	// int alinhamento = (4 - (h.largura * sizeof(Pixel)) % 4) % 4;
 
 	// Ler todos os pixels da imagem
-	for(i=0; i<h.altura; i++) {
-
-
-		for(j=0; j<h.largura; j++) {
-
+	for(i = 0; i < h.altura; i++) {
+		for(j = 0; j < h.largura; j++) {
 			fread(&imagem[i][j], sizeof(Pixel), 1, arquivoEntrada);
-
-			// Converter pra Grayscale
-			grey = imagem[i][j].red * 0.2126 + imagem[i][j].green * 0.7152 + imagem[i][j].blue * 0.0722;
-			imagem[i][j].red = imagem[i][j].green = imagem[i][j].blue = grey;
-			greyscale[i][j] = grey;
 		}
-
 		// fseek(arquivoEntrada, alinhamento, SEEK_CUR);
 	}
 
 	fclose(arquivoEntrada);
 
-	// Gerar um arquivo Greyscale
-	fwrite(&h, sizeof(Header), 1, arquivoSaida);
+	converter_grayscale(imagem, greyscale, numProc, nomeArquivo);
 
-	for (i=0; i<h.altura; i++) {
+	// long double gx, gy;
+	// double p;
+	// // Aplicação do filtro de Sobel
+	// for (i=1; i<h.altura - 1; i++) {
 
-		for (j=0; j<h.largura; j++) {
-			fwrite(&imagem[i][j], sizeof(Pixel), 1, arquivoSaida);
-		}
+	// 	for (j=1; j<h.largura - 1; j++) {
 
-		// for (j=0; j<alinhamento; j++) {
-		// 	fputc(0x00, arquivoSaida);
-		// }
-	}
+	// 		gx = 
+	// 			greyscale[CELL_A] * -1 + greyscale[CELL_B] * 0 + greyscale[CELL_C] * 1
+	// 		+ 	greyscale[CELL_D] * -2 + greyscale[CELL_E] * 0 + greyscale[CELL_F] * 2
+	// 		+ 	greyscale[CELL_G] * -1 + greyscale[CELL_H] * 0 + greyscale[CELL_I] * 1;
 
-	fclose(arquivoSaida);
+	// 		gy = 
+	// 			greyscale[CELL_A] * 1  + greyscale[CELL_B] * 2  + greyscale[CELL_C] * 1
+	// 		+ 	greyscale[CELL_D] * 0  + greyscale[CELL_E] * 0  + greyscale[CELL_F] * 0
+	// 		+ 	greyscale[CELL_G] * -1 + greyscale[CELL_H] * -2 + greyscale[CELL_I] * -1;
 
-	long double gx, gy;
-	double p;
-	// Aplicação do filtro de Sobel
-	for (i=1; i<h.altura - 1; i++) {
-
-		for (j=1; j<h.largura - 1; j++) {
-
-			gx = 
-				greyscale[CELL_A] * -1 + greyscale[CELL_B] * 0 + greyscale[CELL_C] * 1
-			+ 	greyscale[CELL_D] * -2 + greyscale[CELL_E] * 0 + greyscale[CELL_F] * 2
-			+ 	greyscale[CELL_G] * -1 + greyscale[CELL_H] * 0 + greyscale[CELL_I] * 1;
-
-			gy = 
-				greyscale[CELL_A] * 1  + greyscale[CELL_B] * 2  + greyscale[CELL_C] * 1
-			+ 	greyscale[CELL_D] * 0  + greyscale[CELL_E] * 0  + greyscale[CELL_F] * 0
-			+ 	greyscale[CELL_G] * -1 + greyscale[CELL_H] * -2 + greyscale[CELL_I] * -1;
-
-			p = sqrt(gx*gx + gy*gy);
-			sobel[i][j] = p;
-		}
-	}
+	// 		p = sqrt(gx*gx + gy*gy);
+	// 		sobel[CELL_E] = p;
+	// 	}
+	// }
 
 	
-	strcpy(saida, nomeSemExtensao);
-	strcat(saida, "_Sobel");
-	arquivoSaida = fopen(saida, "wb");
+	// strcpy(saida, nomeSemExtensao);
+	// strcat(saida, "_Sobel");
+	// arquivoSaida = fopen(saida, "wb");
+
+	// fwrite(&h, sizeof(Header), 1, arquivoSaida);
+
+
+	// // Pixel temp;
+	// for (i=0; i<h.altura; i++) {
+
+	// 	for (j=0; j<h.largura; j++) {
+	// 		fwrite(&sobel[i][j], sizeof(Pixel), 1, arquivoSaida);
+	// 	}
+
+	// 	// for (j=0; j<alinhamento; j++) {
+	// 	// 	fputc(0x00, arquivoSaida);
+	// 	// }
+		
+	// }
+
+	// fclose(arquivoSaida);
+
+	return 0;
+}
+
+void converter_grayscale(Pixel **imagem, Byte **greyscale, int numProc, char *nomeArq) {
+
+	char saida[50];
+
+	strcpy(saida, nomeArq);
+	saida[strlen(saida) - 4] = '\0'; // Tirar o .bmp do final
+	strcat(saida, "_Greyscale.bmp");
+
+	FILE *arquivoSaida = fopen(saida, "wb");
+	if (arquivoSaida == NULL) {
+		printf("Erro ao salvar a imagem em tons de cinza.\n");
+		exit(0);
+	}
+
+	int i, j, p;
+	int pid, rank = 0;
 
 	fwrite(&h, sizeof(Header), 1, arquivoSaida);
 
-
-	// Pixel temp;
-	for (i=0; i<h.altura; i++) {
-
-		for (j=0; j<h.largura; j++) {
-			fwrite(&sobel[i][j], sizeof(Pixel), 1, arquivoSaida);
+	for (p = 0; p < numProc; p++) {
+		pid = fork();
+		if (pid == 0) {
+			rank = p;
+			break;
 		}
 
-		// for (j=0; j<alinhamento; j++) {
-		// 	fputc(0x00, arquivoSaida);
-		// }
-		
+		calcular(imagem, greyscale, rank, numProc);
+	}
+
+	for (p = 0; p < numProc; p++) { wait(NULL); }
+
+	Pixel temp;
+	for (i = 0; i < h.altura; i++) {
+		for (j = 0; j < h.largura; j++) {
+			temp.red = temp.green = temp.blue = greyscale[i][j];
+			// if (j != 0 && i % j == 360) {
+				printf("%u\t", greyscale[i][j]);
+			// }
+			fwrite(&temp, sizeof(Pixel), 1, arquivoSaida);
+		}
 	}
 
 	fclose(arquivoSaida);
+}
 
-	return 0;
+void calcular(Pixel **imagem, Byte **greyscale, int rank, int numProc) {
+
+	int linha, coluna;
+	Byte grey;
+
+	// Converter pra Grayscale
+	for (linha = rank; linha < h.altura; linha += numProc) {
+		for (coluna = rank; coluna < h.largura; coluna += numProc) {
+			grey = imagem[linha][coluna].red * 0.2126 
+				 + imagem[linha][coluna].green * 0.7152 
+				 + imagem[linha][coluna].blue * 0.0722;
+			
+			greyscale[linha][coluna] = grey;
+		}
+	}
 }
